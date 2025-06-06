3ad2db296292dfd9b3126a6c889e0ea85be4d948
RafaelPozoyan <114441417+RafaelPozoyan@users.noreply.github.com>
2025-06-06 20:05:45 +0300

    Delete prj.cw directory

--- prj.cw/include/image_processor.h
+++
@@ -1,9 +0,0 @@
-#include <opencv2/opencv.hpp>
-
-class ImageProcessor {
-public:
-    ImageProcessor();
-    cv::Mat preprocessImage(const cv::Mat& input_image);
-
-    cv::Mat enhanceContrast(const cv::Mat& input_image);
-};
