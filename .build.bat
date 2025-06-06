f8d7fb5d5f77dd909f1371109ec935044f343991
RafaelPozoyan <114441417+RafaelPozoyan@users.noreply.github.com>
2025-06-06 20:15:13 +0300

    Update .build.bat

--- .build.bat
+++ .build.bat
@@ -1,1 +1,2 @@
-C:\Users\pozoy\source\repos\misis2025s-3-pozoyan_r_o>cmake -S . -B ./build -DCMAKE_TOOLCHAIN_FILE=C:/Users/pozoy/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake -S . -B ./build -DCMAKE_TOOLCHAIN_FILE=C:/Users/pozoy/vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake --build ./build
